3e81bbfed6674655585ee52a0dce6908b3e7e70d
Grego Dadone <grego.dadone@gmail.com>
2020-06-11 19:12:18 -0300

    First commit

---
+++ src/main/java/com/gregodadone/multiplebranchofficesappointmentschedulerbackend/MultipleBranchOfficesAppointmentSchedulerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gregodadone.multiplebranchofficesappointmentschedulerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleBranchOfficesAppointmentSchedulerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleBranchOfficesAppointmentSchedulerBackendApplication.class, args);
+    }
+
+}
